76f631107c88ca800a98206af61f68741eef704e
Caner Al <caneral@yaani.com>
2021-05-22 23:13:55 +0300

    meet listeleme eklendi

--- StudentInfo.WebApi/Startup.cs
+++ StudentInfo.WebApi/Startup.cs
@@ -82,14 +82,14 @@ namespace StudentInfo.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                //app.UseSwagger(c =>
-                //{
-                //    c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
-                //    {
-                //        swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"https://{"/vbsadmin"}" } };
-                //    });
+                app.UseSwagger(c =>
+                {
+                    c.PreSerializeFilters.Add((swaggerDoc, httpReq) =>
+                    {
+                        swaggerDoc.Servers = new List<OpenApiServer> { new OpenApiServer { Url = $"https://{"/vbsadmin"}" } };
+                    });
 
-                //});
+                });
                 app.UseSwagger();
                 app.UseSwaggerUI(c =>
                 {
